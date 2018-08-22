9a76017a38d9d0a2c1107ded1b28cf9ab28c37bb
karra sankar <karrasankar09@gmail.com>
2018-08-22 15:15:20 +0530

    MARRIAGEAPP-USING-HTML-JS

---
+++ MariageApp2-Form_Validations-JAVASCRIPT-AND-HTML/src/com/nt/servlet/CheckMarriageEligibilityServlet.java
@@ -0,0 +1,193 @@
+package com.nt.servlet;
+/*
+ * you copy and paste 
+ * change name
+ * properties-->right click on project
+ * web project Settings change name
+ * DISABLE JAVASCRIPT IN CHROME:
+ * settings-->web browser-->javascript-->disable
+ * DISABLE JAVASCRIPT IN FIREFOX:
+ * search-->about config-->javascript-->disable.
+ */
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class CheckMarriageEligibilityServlet extends HttpServlet {
+
+	
+	     @Override
+		public void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+			
+			//PrintWriter pw=null;
+			
+			String name=null,gender=null;
+			
+			String tage=null;
+			
+			int age=0;
+			
+			ArrayList<String> errList=null;
+			
+			String vstatus=null;
+			
+			//General settings,Get PrintWriter
+			
+			final PrintWriter pw=res.getWriter();
+			
+			//set ResponseContentType
+			
+			res.setContentType("text/html");
+			
+			//Get request parameter query String values(form data),read form data 
+			
+			name=req.getParameter("pname");
+			
+			gender=req.getParameter("gen");
+			
+			
+			
+			
+			//age=Integer.parseInt(req.getParameter("page"));
+			
+			tage=req.getParameter("page");
+			
+			
+			
+			errList=new ArrayList();
+			
+			//age=Integer.parseInt("page");
+			
+			//read vflage value to know that status 
+			
+			vstatus = req.getParameter("vflag");
+			
+			if (vstatus.equalsIgnoreCase("no")) {
+			//Server Side form Validation Logic
+			
+			System.out.println("Server Side form validations....");
+			
+			if(name==null || name.length()==0 || name.equals("") ) {
+				
+				errList.add("Person Name Is Required");
+				
+				return;
+			}
+			
+			
+			if(tage==null || tage.length()==0 || tage.equals("")) {
+				
+				errList.add("Person Age Is Required");
+				
+				return;
+			}
+			
+			else {
+				
+				try {
+					
+					age=Integer.parseInt(tage);
+					
+				if(age<1 || age >125) {
+					
+						errList.add("Person Age must be 1 to 125");
+						
+						return;
+					}
+				}
+					catch(NumberFormatException nfe) {
+						
+						errList.add("Person Age must be Numeric value");
+						
+						return;
+					}
+				}
+			
+		/*	//if(errList.isEmpty()) {  
+			
+			if(errList.size()>0) {
+							
+				for(String err : errList) {
+					
+					pw.println("<li><b>"+err+"</b></li>");
+				}
+				
+				return;
+			}
+			
+			else {
+				
+				errList.forEach((err) ->
+				
+					pw.println("Error MEssage")
+					
+						);
+				
+			}*/
+			}
+			else {
+				age=Integer.parseInt(tage);
+			
+			}
+			
+			
+			//Write Request processing logic
+			
+			if(gender.equalsIgnoreCase("M"))
+				
+				if(age>=21)	{
+				
+	
+				pw.println("<h1 style='color:cyan;text-align:center'>Mr."+name+"  you are Boy/Man  Eligible for Marriage, But still Think once.</h1>");
+
+		}
+		
+		else {
+			
+		
+			pw.println("<h1 style='color:maroon;text-align:center'>Mr."+name+"  you are Boy/Man  not Eligible for Marriage, Feel happy</h1>");
+
+		}
+			if(gender.equalsIgnoreCase("F"))
+				
+				if(age>=18) {
+					
+					pw.println("<h1 style='color:cyan;text-align:center'>Miss."+name+"  you are Girl/Woman  Eligible for Marriage, But Keep your Terms and Conditions.</h1>");
+				}
+			
+				else {
+					
+					pw.println("<h1 style='color:maroon;text-align:center'>Miss."+name+"  you are GIRL/Woman not Eligible for Marriage, Feel Double Happy and make Others Happy</h1>");
+
+				}
+			
+		
+		//add Hyberlink
+		
+		
+		
+		//pw.println("<br><a href='http://localhost:3030/MarriageApp/input.html'>Home</a>");
+		pw.println("<br><a href='input.html'>Home</a>");
+		
+		//close Stream
+		
+		pw.close();
+	}
+		@Override
+		public void doPost(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+			doGet(req,res);
+		}
+
+}
