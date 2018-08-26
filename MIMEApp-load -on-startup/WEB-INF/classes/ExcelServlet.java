4da1779f14d867c4a7d1f8132aae2fd2ce16ea4e
karra sankar <karrasankar09@gmail.com>
2018-08-26 07:37:42 +0530

    MIMEAPP-LOAD-ON-STARTUP

---
+++ MIMEApp-load -on-startup/WEB-INF/classes/ExcelServlet.java
@@ -0,0 +1,86 @@
+//ExcelServlet.java
+/*
+
+<tr>-->table row
+<th>-->table header
+<td>-->table cell or standard cell
+*/
+package com.nt.servlet;
+
+import javax.servlet.*;
+
+
+import javax.servlet.http.*;
+
+import java.io.*;
+
+	public class ExcelServlet extends HttpServlet 
+
+	{
+		static {
+		
+		System.out.println("ExcelServlet:static block");
+		
+	}//static block
+
+	public ExcelServlet() {
+		
+		System.out.println("ExcelServlet:0-param constructor");
+		
+	}//constructor
+	
+	public void init(ServletConfig cg)
+	
+	{
+		
+		System.out.println("ExcelServlet:init(-)");
+		
+	}//init(-)
+	
+		protected void service(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
+			
+			System.out.println("ExcelServlet:servic(-,-)");
+		
+			//get PrintWriter object
+		
+			PrintWriter pw=null;
+		
+			pw=res.getWriter();
+		
+			//set response ContentType 
+		
+			res.setContentType("application/vnd.ms-excel");
+		
+			//Write logic to generate output (webpages),Display Content
+		
+			pw.println("<table border='1' bgcolor='cyan'>");
+		
+		pw.println("<tr>  <th>Show Name</th>    <th>Host</th>  </tr>");
+		
+		pw.println("<tr>  <td>Big Boss-Telugu</td>  <td>Natural Star-Nani</td>  </tr>");
+		
+		pw.println("<tr>  <td>Big Boss-Tamil</td>  <td>Movie Navel-Kamal Hasan</td>  </tr>");
+		
+		pw.println("<tr>  <td>Big Boss-Marathi</td>  <td>The Foriegn Villan-Mahesh</td>   </tr>");
+		
+		pw.println("<tr>  <td>Big Boss-Hindi</td>  <td>Chickora Star-Salman Khan</td>   </tr>");
+		
+		/*pw.println("<tr>  <th>Player</th>  <th>Role</th></tr>");
+		
+		pw.println("<tr>  <td>Dhoni</td>  <td>Captain</td>  </tr>");
+		
+		pw.println("<tr>  <td>Sachin</td>  <td>All Rounder</td>  </tr>");
+		
+		pw.println("<tr>  <th>Habits</th>  <th>Role</th></tr>");
+		
+		pw.println("<tr>  <td>Reading </td>  <td>books</td> </tr>");
+		
+		pw.println("<tr>  <td>cricket</td>  <td>All Rounder</td>  </tr>");*/
+		
+		pw.println("</table>");
+		
+		//close Streams 
+		
+	}//service
+
+};//class
